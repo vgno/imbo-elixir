da4f5d5537416858e4899b1aa18ed77a486d8ade
Petter Kaspersen <petter.hogmo.kaspersen@vg.no>
2019-12-17 10:39:53 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ImboConnector.MixProject do
   def project do
     [
       app: :imbo_connector,
-      version: "2.1.0",
+      version: "2.3.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
